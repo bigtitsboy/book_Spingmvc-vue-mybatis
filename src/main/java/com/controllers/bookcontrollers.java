ffbd3778147ee21c6c1e203375b848bcab3668e1
bigtitsboy <ow20000821@outlook.com>
2021-09-15 10:15:34 +0800

    2021/9/15

--- src/main/java/com/controllers/bookcontrollers.java
+++ src/main/java/com/controllers/bookcontrollers.java
@@ -5,13 +5,10 @@ import books.dao.StudentDao;
 import books.pojo.Book;
 import books.pojo.Student;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Controller
 @RequestMapping("/book")
 public class bookcontrollers {
